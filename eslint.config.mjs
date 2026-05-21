c236731fcf5734c8369f4aba2252b64a0cd20b9c
adamsbarry18 <mabarry2018@gmail.com>
2026-05-21 15:13:43 +0200

    feat: fix eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig([{
+    extends: [...nextCoreWebVitals, ...nextTypescript],
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'warn',
+        'react-hooks/set-state-in-effect': 'warn',
+        'react/no-unescaped-entities': 'warn',
+        'prefer-const': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-require-imports': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
+    },
+}]);
